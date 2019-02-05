8117d264f46a15368a0f69f69d86022df030b859
Cameron Ray <cameron@cameronray.net>
2019-02-05 00:36:00 -0500

    Update language

--- src/client/scenes/layout.jsx
+++ src/client/scenes/layout.jsx
@@ -16,8 +16,7 @@ export const Layout = () => {
         </div>
       </div>
       <footer className="footer">
-        Fork on <a href="https://github.com/altangent/cryptolist">GitHub</a> | Powered by{' '}
-        <a href="https://blocktap.io">Blocktap</a>
+        Fork on <a href="https://github.com/camray/webapi-playground">GitHub</a>
       </footer>
     </div>
   );
